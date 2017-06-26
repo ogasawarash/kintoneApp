ee261259a2e5b3dcb8d25e4e8320585913bab906
ogasawarash <ogasawarash@gmail.com>
2017-06-26 14:42:12 +0900

    レコード作成で「私物PC利用～」を非表示

--- ownPCgroup_showhide.js
+++ ownPCgroup_showhide.js
@@ -6,12 +6,6 @@
   ];
   kintone.events.on(events, function(event) {
     var record = event.record;
-    if (record['radio_chooseAccount']['value'] === 'C. 私物PCを利用したSSL-VPN接続許可' &&
-  record['レコード']['value'] === 'チェックリスト確認中') {
-      kintone.app.record.setFieldShown('group_ownPCenv', true);
-      kintone.app.record.setGroupFieldOpen('group_ownPCenv', true);
-    } else {
       kintone.app.record.setFieldShown('group_ownPCenv', false);
-    }
   });
 })();
