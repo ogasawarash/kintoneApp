6213b8f53f7760e8c93c996e392ae377da21099b
ogasawarash <ogasawarash@gmail.com>
2017-06-26 18:19:54 +0900

    1つ目のチェックボックスがチェック済みだとエラーが表示されない

--- checkbox_allchecked.js
+++ checkbox_allchecked.js
@@ -3,14 +3,14 @@
 
   function notCheckedAll(event) {
     var record = event.record;
-    var error = event.error;
+    var noerror = event.error;
     var check = 'すべての項目を確認しました。'
     if (record['checkbox_allCheck']['value'][0] !== check) {
       record['checkbox_allCheck']['error'] = "必須項目を入力してください";
       event.error ="必須項目を入力してください";
       return event;
     }else{
-      event.error = error;
+      event.error = noerror;
     }
   }
   kintone.events.on('app.record.edit.submit', notCheckedAll);
--- checkbox_errorchecked.js
+++ checkbox_errorchecked.js
@@ -3,7 +3,7 @@
 
   function cancelSave(event) {
     var record = event.record;
-    var error = event.error;
+    var noError = event.error;
     var check = [
       '利用PCが、家族と共用しない自分専用のPCになっていること やむを得ず利用予定PCを家族と共用する場合、業務利用用にアカウントを分けられていること',
       '利用予定PCのアカウントには、パスワードが設定されていること',
@@ -12,13 +12,24 @@
       '公衆無線LANサービスなどの暗号化されていない、盗聴の恐れがあるネットワークでは使用しないこと',
       '会社の自PCへリモートデスクトップ接続しての作業のみ行うこと'
     ];
-    for (var i = 0; i < 6 ; i++) {
+    var allCheck = 'すべての項目を確認しました。'
+    var pleaseCheck = "必須項目を入力してください"
+    for (var i = 0; i < 6; i++) {
+      if (record['checkbox_allCheck']['value'][0] !== allCheck) {
+        record['checkbox_allCheck']['error'] = pleaseCheck;
+        event.error = pleaseCheck;
+      }
       if (record['checkbox_checkList']['value'][i] !== check[i]) {
-        record['checkbox_checkList']['error'] = "必須項目を入力してください";
-        event.error = "必須項目を入力してください";
+        record['checkbox_checkList']['error'] = pleaseCheck;
+        event.error = pleaseCheck;
         return event;
       } else {
-        event.error = error;
+        if (record['checkbox_allCheck']['value'][0] !== allCheck) {
+          record['checkbox_allCheck']['error'] = pleaseCheck;
+          event.error = pleaseCheck;
+          return event;
+        }
+        event.error = noError;
       }
     }
   }
