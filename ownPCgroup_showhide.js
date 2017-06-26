dc0a5227862776446fb27127d8ce4aa02ff14b5c
ogasawarash <ogasawarash@gmail.com>
2017-06-26 16:39:23 +0900

    ラジオボタン、ステータスによるgroupの表示・非表示OK

--- ownPCgroup_showhide.js
+++ ownPCgroup_showhide.js
@@ -9,10 +9,10 @@
   var indicateStatus = 'チェックリスト確認中'; 
   kintone.events.on(events, function(event) {
       var record = event.record;
-      if (record['radio_chooseAccount']['value'] === 'C. 私物PCを利用したSSL-VPN接続許可') {
-          kintone.app.record.setFieldShown('group_ownPCenv', true);
-          } else {
+      if (record['radio_chooseAccount']['value'] === 'C. 私物PCを利用したSSL-VPN接続許可' && record['ステータス']['value'] === 'チェックリスト確認中'){
+           kintone.app.record.setFieldShown('group_ownPCenv', true);
+          }else{
           kintone.app.record.setFieldShown('group_ownPCenv', false);
-          }
+      }
   });
 })();
