383a967825536175624c9f5b40b102b38330d288
ogasawarash <ogasawarash@gmail.com>
2017-06-26 16:23:59 +0900

    ラジオボタンでCが選ばれた時のみ表示」まで

--- ownPCgroup_showhide.js
+++ ownPCgroup_showhide.js
@@ -9,6 +9,10 @@
   var indicateStatus = 'チェックリスト確認中'; 
   kintone.events.on(events, function(event) {
       var record = event.record;
-           kintone.app.record.setFieldShown('group_ownPCenv', false);
+      if (record['radio_chooseAccount']['value'] === 'C. 私物PCを利用したSSL-VPN接続許可') {
+          kintone.app.record.setFieldShown('group_ownPCenv', true);
+          } else {
+          kintone.app.record.setFieldShown('group_ownPCenv', false);
+          }
   });
 })();
