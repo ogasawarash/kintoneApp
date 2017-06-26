468333c1ccc618afb6d20d21d8b048a9794efb93
ogasawarash <ogasawarash@gmail.com>
2017-06-26 16:14:58 +0900

    全ての場合においてgroupを非表示

--- ownPCgroup_showhide.js
+++ ownPCgroup_showhide.js
@@ -1,11 +1,14 @@
 (function() {
-  "use strict";
+ "use strict";
 
   var events = [
-    'app.record.create.show',
+    'app.record.detail.show',
+    'app.record.edit.show',
+    'app.record.detail.process.proceed'
   ];
+  var indicateStatus = 'チェックリスト確認中'; 
   kintone.events.on(events, function(event) {
-    var record = event.record;
-      kintone.app.record.setFieldShown('group_ownPCenv', false);
+      var record = event.record;
+           kintone.app.record.setFieldShown('group_ownPCenv', false);
   });
 })();
